98d8e3f5056cd197348c3808ba91409e37e40ffe
Jorge Arturo Rodriguez Martin <joarceus@hotmail.com>
2019-08-25 01:28:25 -0500

    init

---
+++ Tarea1/Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace Tarea1
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnLoadText = new System.Windows.Forms.Button();
+            this.txtBoxFirst = new System.Windows.Forms.TextBox();
+            this.txtBoxSecond = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.brnLoad = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnLoadText
+            // 
+            this.btnLoadText.Location = new System.Drawing.Point(338, 81);
+            this.btnLoadText.Name = "btnLoadText";
+            this.btnLoadText.Size = new System.Drawing.Size(114, 38);
+            this.btnLoadText.TabIndex = 0;
+            this.btnLoadText.Text = "Cargar texto";
+            this.btnLoadText.UseVisualStyleBackColor = true;
+            this.btnLoadText.Click += new System.EventHandler(this.btnLoadText_Click);
+            // 
+            // txtBoxFirst
+            // 
+            this.txtBoxFirst.Location = new System.Drawing.Point(52, 30);
+            this.txtBoxFirst.Multiline = true;
+            this.txtBoxFirst.Name = "txtBoxFirst";
+            this.txtBoxFirst.Size = new System.Drawing.Size(189, 153);
+            this.txtBoxFirst.TabIndex = 1;
+            // 
+            // txtBoxSecond
+            // 
+            this.txtBoxSecond.Location = new System.Drawing.Point(547, 30);
+            this.txtBoxSecond.Multiline = true;
+            this.txtBoxSecond.Name = "txtBoxSecond";
+            this.txtBoxSecond.ReadOnly = true;
+            this.txtBoxSecond.Size = new System.Drawing.Size(189, 153);
+            this.txtBoxSecond.TabIndex = 2;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(184, 254);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(459, 150);
+            this.dataGridView1.TabIndex = 3;
+            // 
+            // brnLoad
+            // 
+            this.brnLoad.Location = new System.Drawing.Point(338, 166);
+            this.brnLoad.Name = "brnLoad";
+            this.brnLoad.Size = new System.Drawing.Size(114, 41);
+            this.brnLoad.TabIndex = 4;
+            this.brnLoad.Text = "Cargar";
+            this.brnLoad.UseVisualStyleBackColor = true;
+            this.brnLoad.Click += new System.EventHandler(this.brnLoad_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.brnLoad);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.txtBoxSecond);
+            this.Controls.Add(this.txtBoxFirst);
+            this.Controls.Add(this.btnLoadText);
+            this.Name = "MainForm";
+            this.Text = "Main";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnLoadText;
+        private System.Windows.Forms.TextBox txtBoxFirst;
+        private System.Windows.Forms.TextBox txtBoxSecond;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button brnLoad;
+    }
+}
+
---
+++ Tarea1/Form1.cs
@@ -0,0 +1,251 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Tarea1
+{
+    public partial class MainForm : Form
+    {
+        const int dataRows = 3;
+        public MainForm()
+        {
+            InitializeComponent();
+            createDataGrid();
+        }
+
+        private enum States{
+            Start,
+            Equal,
+            Number,
+            Word,
+            Point,
+            Float,
+            GreaterThan,
+            Less,
+            And,
+            Or
+        }
+
+        private List<String> reservedWords = new List<String>(new string[] { "if", " while ", " for", " switch ", " return ", " else " });
+        private List<String> Types = new List<String>(new string[] { " int" ," void " , " char " , " double " , " float " , " bool " });
+
+        private void btnLoadText_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog myOpener = new OpenFileDialog();
+            myOpener.Filter = "Text|*.txt|All|*.*";
+            Stream myStream;
+        
+            try
+            {
+                if (myOpener.ShowDialog() == DialogResult.OK)
+                {
+                    if((myStream = myOpener.OpenFile()) != null)
+                    {
+                        loadToTextBox(myStream, myOpener);
+                    }
+                    
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Something went wrong");
+            }
+        }
+
+        private void loadToTextBox(Stream myStream, OpenFileDialog myOpener)
+        {
+            if ((myStream = myOpener.OpenFile()) != null)
+            {
+                string strFileName = myOpener.FileName;
+                string content = File.ReadAllText(strFileName);
+
+                txtBoxFirst.Text = content;
+            }
+        }
+
+        private void setDataToGrid()
+        {
+           
+            string[] words = txtBoxFirst.Text.Split(new string[] { " ", "\r\n" }, StringSplitOptions.RemoveEmptyEntries);
+            int index = 0;
+
+            while (index < words.Length)
+            {
+                string[] adder = new string[dataRows];
+                for (int i = 0; i < dataRows; i++)
+                {
+                    if(index >= words.Length) { break; }
+                    adder[i] = words[index];
+                    index++;
+                }
+                dataGridView1.Rows.Add(adder);
+            }
+            MessageBox.Show("Numero de palabras; " + words.Length);
+            
+        }
+
+        private void createDataGrid()
+        {
+            dataGridView1.ColumnCount = dataRows;
+            dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+        }
+
+        private void brnLoad_Click(object sender, EventArgs e)
+        {
+            dataGridView1.Rows.Clear();
+            txtBoxSecond.Text = txtBoxFirst.Text;
+            evaluateCharacter(txtBoxFirst.Text);
+
+
+        }
+
+        private void evaluateCharacter(string data)
+        {
+            States myState = States.Start;
+            string union = null;
+
+
+            foreach(char myChar in data){
+
+                switch (myState)
+                {
+                    case States.Start:
+
+                        if (Char.IsLetter(myChar))
+                        {
+                            union += myChar;
+                            myState = States.Word;
+                        }
+
+                        else if (Char.IsNumber(myChar))
+                        {
+                            union += myChar;
+                            myState = States.Number;
+                        }
+
+                        else if (myChar == '{')
+                        {
+                            dataGridView1.Rows.Add(new string[] { "{", "llave izq", "id: 6" });
+                        }
+
+                        else if (myChar == '}')
+                        {
+                            dataGridView1.Rows.Add(new string[] { "}", "llave der", "id: 7" });
+                        }
+
+                        else if (myChar == '(')
+                        {
+                            dataGridView1.Rows.Add(new string[] { "(", "parentesis izq", "id: 4" });
+                        }
+
+                        else if (myChar == ')')
+                        {
+                            dataGridView1.Rows.Add(new string[] { ")", "parentesis der", "id: 5" });
+                        }
+
+                        else if (myChar == ';')
+                        {
+                            dataGridView1.Rows.Add(new string[] { ";", "punto y coma", "id: 2" });
+                        }
+
+                        else if (myChar == '+')
+                        {
+                            dataGridView1.Rows.Add(new string[] { "+", "suma", "id: 5" });
+                        }
+
+                        else if (myChar == '-')
+                        {
+                            dataGridView1.Rows.Add(new string[] { "-", "resta", "id: 17" });
+                        }
+
+                        else if (myChar == '*')
+                        {
+                            dataGridView1.Rows.Add(new string[] { "*", "multiplicación", "id: 16" });
+                        }
+
+                        else if (myChar == '/')
+                        {
+                            dataGridView1.Rows.Add(new string[] { "/", "division", "id: 16" });
+                        }
+
+                        else if (myChar == '=')
+                        {
+                            union += myChar;
+                            myState = States.Equal;
+                        }
+
+                        else if (myChar == '>')
+                        {
+                            union += myChar;
+                            myState = States.GreaterThan;
+                        }
+
+                        else if (myChar == '<')
+                        {
+                            union += myChar;
+                            myState = States.Less;
+                        }
+
+                        else if (myChar == '&')
+                        {
+                            union += myChar;
+                            myState = States.And;
+                        }
+
+                        else if (myChar == '|')
+                        {
+                            union += myChar;
+                            myState = States.Or;
+                        }
+
+                        break;
+
+                    case States.Number:
+
+                    break;
+
+                    case States.Word:
+
+                    break;
+
+                    case States.Point:
+
+                    break;
+
+                    case States.Float:
+
+                    break;
+
+                    case States.GreaterThan:
+
+                    break;
+
+                    case States.Less:
+
+                    break;
+
+                    case States.And:
+
+                    break;
+
+                    case States.Or:
+
+                    break;
+
+                }
+
+            }
+            
+            
+            
+            
+        }
+    }
+}
